ab151b92dcf8373955414109a65c54b4056fd09d
gmp0109 <pmonse386@gmail-.com>
2026-07-02 00:04:44 -0400

    Add user registration feature

---
+++ server/frontend/src/App.1.js
@@ -0,0 +1,11 @@
+import LoginPanel from "./components/Login/Login";
+import { Routes, Route } from "react-router-dom";
+
+export function App() {
+    return (
+        <Routes>
+            <Route path="/login" element={<LoginPanel />} />
+            <Route path="/register" element={<Register />} />
+        </Routes>
+    );
+}
